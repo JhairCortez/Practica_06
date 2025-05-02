93bb8de5ddfd9abbb93c1ab3578f9c1626134fb8
JhairCortezUcb <jhair.cortez@ucb.edu.bo>
2025-05-02 17:32:11 -0400

    Update Ejercicio_01.cpp

--- Ejercicio_01.cpp
+++ Ejercicio_01.cpp
@@ -1,1 +1,16 @@
+#include<iostream>
+using namespace std;
 
+void mostrarAscendente(int n)
+{
+   if(n > 1)
+      mostrarAscendente(n - 1)
+   cout << n << " "
+}
+
+int main()
+{
+   int n
+   cin >> n
+   mostrarAscendente(n)
+}
