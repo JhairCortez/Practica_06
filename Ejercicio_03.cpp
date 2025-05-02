58b171ef2f523510964692a863621beb0820d3a7
JhairCortezUcb <jhair.cortez@ucb.edu.bo>
2025-05-02 17:33:18 -0400

    Create Ejercicio_03.cpp

---
+++ Ejercicio_03.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+void cuadradosAscendente(int n)
+{
+   if(n > 1)
+      cuadradosAscendente(n - 1)
+   cout << n * n << " "
+}
+
+int main()
+{
+   int n
+   cin >> n
+   cuadradosAscendente(n)
+}
