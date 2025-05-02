1da24d90996a8365f8a7b2559b5c7152f0211c5a
JhairCortezUcb <jhair.cortez@ucb.edu.bo>
2025-05-02 17:36:13 -0400

    Update Ejercicio_10.cpp

--- Ejercicio_10.cpp
+++ Ejercicio_10.cpp
@@ -1,1 +1,19 @@
+#include<iostream>
+using namespace std;
 
+void invertirNumero(int n, int reverso = 0)
+{
+   if(n == 0)
+   {
+      cout << reverso
+      return
+   }
+   invertirNumero(n / 10, reverso * 10 + n % 10)
+}
+
+int main()
+{
+   int n
+   cin >> n
+   invertirNumero(n)
+}
